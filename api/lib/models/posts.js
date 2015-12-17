fee36d3d9dc22c0bd04952a1fccb350624159266
Arjun Ganesan <arjun@arjun-g.com>
2015-12-17 19:58:21 +0530

    Completed adding post method

--- api/lib/models/posts.js
+++ api/lib/models/posts.js
@@ -1,29 +1,44 @@
 var mongo = require('mongodb'),
 	ObjectId = mongo.ObjectId,
 	_ = require("underscore")._;
-	
+
 module.exports = function(db){
 	var collection = db.collection("posts");
-	
+
 	var model = {};
-	
+
+	model.createSlug = function(tries){
+		var slug = _.sample("abcdefghijklmnopqrstuvwxyz", 3).join('') + "-" + _.sample("0123456789", 4).join('');
+		return new Promise((resolve, reject) => {
+			collection.findOne({slug: slug}).then(function(doc){
+				if(!doc) resolve(slug);
+				else if(_.isUndefined(tries)) model.createSlug(1).then(resolve, reject);
+				else if(tries === 5) reject("Iteration Count Exceeded");
+				else model.createSlug(++tries).then(resolve, reject);
+			});
+		});
+	};
+
 	model.get = function(postId){
 		return collection.findOne({_id: ObjectId(postId)});
 	};
-	model.post = function(post){
+	model.post = function(postObj){
+		var post = Object.assign({}, postObj);
 		if(!post.content || !post.author || !post.position){
-			return Promise.reject("Required Fields Missing");
+			return Promise.reject(new Error("Required Fields Missing"));
 		}
-		post.slug = _.sample("abcdefghijklmnopqrstuvwxyz", 3).join('') + "-" + _.sample("0123456789", 4).join('');
 		post.createdOn = new Date();
-		return collection.findOne({ slug: post.slug })
-		.then(function(data){
-			if(!data)
-				return collection.insertOne(post);
-			else
-				return null;
-		});
+		return model.createSlug()
+		.then(slug => {
+			post.slug = slug;
+			return post;
+		})
+		.then(post => collection.insertOne(post));
 	};
-	
+	model.search = function(location, radius){
+		var post = Object.assign({}, postObj);
+
+	}
+
 	return model;
 }
