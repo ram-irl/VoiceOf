72a99057f20eed641a2ea602374ab0292b7f6006
Manikandan G <gmanikandan@vthink.co.in>
2015-12-18 18:50:16 +0530

    google api http added

--- clients/website/public/js/factories/apiFactory.js
+++ clients/website/public/js/factories/apiFactory.js
@@ -1,17 +1,19 @@
 angular.module('voiceOf.factories').factory("api", ['$http', 'CONSTANTS', function ($http, CONSTANTS) {
         var service = {};
-        service.httpRequest = function (method, path, header, data, callback) {
+        service.httpRequest = function (method, path, data, callback) {
 //            if (http == null)
 //                callback({
 //                    error: true,
 //                    errorCode: "HTTP_NULL"
 //                }, null);
 //            loadHeaders();
-//            _apiHeaders['Content-Type'] = 'application/json';
+//            _apiHeaders['Content-Type'] = 'application/json';           
             $http({
                 method: method,
                 url: path,
-                headers: header,
+                headers: {'Content-Type': 'application/json',
+                    Authorization: 'VOICEOF-AUTH token="'+getCookie('userSessionToken')+'", AppId="WebsiteApp1"'
+                },
                 data: data
             }).success(function (data, status, headers, config) {
                 if (data.error) {
@@ -27,6 +29,25 @@ angular.module('voiceOf.factories').factory("api", ['$http', 'CONSTANTS', functi
             });
         };
         
+        //Google request
+        service.googleRequest = function (method, path, callback) {
+            $http({
+                method: method,
+                url: path
+            }).success(function (data) {
+                if (data.error) {
+                    callback(data, null);
+                } else {
+                    callback(null, data);
+                }
+            }).error(function (data) {
+                callback({
+                    error: true,
+                    errorCode: "UNKNOWN_ERROR"
+                }, null);
+            });
+        };
+
         //Get all post
         service.getAllpost = function (callback) {
 //            this.httpRequest("POST", "/users", null, null, function (err, data) {
@@ -36,11 +57,19 @@ angular.module('voiceOf.factories').factory("api", ['$http', 'CONSTANTS', functi
 //                    callback(null, data);
 //            });
         };
-        
-        //Submit post
 
-        service.checkLocationAvailable = function (callback) {         
-            this.httpRequest("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("#addressMsg").val() + "&key=AIzaSyBTznaZuJw6VKOEACAZENeAabe1MGswaEM", null, null, function (err, data) {
+        //Submit post
+        service.submitPost = function () {
+            this.httpRequest("POST", "/users", null, function (err, data) {
+                if (err)
+                    callback(err, null);
+                else
+                    callback(null, data);
+            });
+        };
+
+        service.checkLocationAvailable = function (callback) {
+            this.googleRequest("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("#addressMsg").val() + "&key=AIzaSyBTznaZuJw6VKOEACAZENeAabe1MGswaEM", function (err, data) {
                 if (err)
                     callback(err, null);
                 else
