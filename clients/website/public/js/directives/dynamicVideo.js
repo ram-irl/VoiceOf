991cb6b70efeb73f8e222f4fede7d228048ae9a2
Kumaresan <kumaresan@vthink.co.in>
2015-12-19 19:55:14 +0530

    video issue fixed

--- clients/website/public/js/directives/dynamicVideo.js
+++ clients/website/public/js/directives/dynamicVideo.js
@@ -6,21 +6,30 @@ voiceOf.directive('dynamicUrl', function () {
         scope: {
             url: "=url"
         },
-        controller: function($scope){
-            $scope.getFileExtension = function(url) {
-                if(url && url!==null && url.length>0){                    
-                    var dotIndex = url.lastIndexOf('.');
-                    if(dotIndex<0)return "";
-                    return url.substring(dotIndex+1);
-                }                   
-                else return "";
-            };
-        },
         link: function ($scope, element, $window) {
             //$scope.url = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4";
-            element.attr('src', $scope.url);
-            var ext = $scope.getFileExtension($scope.url).toLowerCase();
-            element.attr('type', 'video/'+ext);
+
+            $scope.$watch('url', function (newval, oldval) {
+                $scope.loadVideo();
+            });
+
+            $scope.loadVideo = function () {
+                if (!$scope.url)
+                    return;
+                $(element).attr('src', $scope.url);
+                var ext = $scope.getFileExtension($scope.url).toLowerCase();
+                $(element).attr('type', 'video/' + ext);
+            };
+
+            $scope.getFileExtension = function (url) {
+                if (url && url !== null && url.length > 0) {
+                    var dotIndex = url.lastIndexOf('.');
+                    if (dotIndex < 0)
+                        return "";
+                    return url.substring(dotIndex + 1);
+                } else
+                    return "";
+            };
         }
     };
 });
